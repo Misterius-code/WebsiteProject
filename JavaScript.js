359d7866ee4280111305a7d2ce8001ba83116260
Misterius-code <55873838+Misterius-code@users.noreply.github.com>
2020-05-26 02:44:01 +0100

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,40 @@
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+		
+<script type="text/javascript">
+
+    var numer = Math.floor(Math.random()*5)+1;
+    
+    var timer1 = 0;
+    var timer2 = 0;
+    
+    function ustawslajd(nrslajdu)
+    {
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+        numer = nrslajdu - 1;
+        
+        schowaj();
+        setTimeout("zmienslajd()", 500);
+        
+    }
+    
+    function schowaj()
+    {
+        $("#slider").fadeOut(500);
+    }
+
+    function zmienslajd()
+    {
+        numer++; if (numer>5) numer=1;
+        
+        var plik = "<img src=\"slajdy/slajd" + numer + ".png\" />";
+        
+        document.getElementById("slider").innerHTML = plik;
+        $("#slider").fadeIn(500);
+        
+        timer1 = setTimeout("zmienslajd()", 5000);
+        timer2 = setTimeout("schowaj()", 4500);
+    
+    }
+    
+</script>
---
+++ documentation.md
@@ -0,0 +1,13 @@
+                    Report
+
+Introduction
+I was asked to make a website and solve world problem. So in
+                Solve World Problem? Yeah help chicken save his world!
+  But now seriosuly. The problem with i will tugle today is self promotion which is a big issue. It might
+                come with different varioriates
+                such as selfpromotion as influener buisnessman , developer , bloger and youtuber. each of these jobs
+                requires
+                It is important to promote yourself in order to gain new customers. There are many ways to promote such
+                as the most power full like social media to even poczta pantelfowa
+                They helps to get new customers and stay updated with already existing ones. but everyone need a
+                website. Do you even exist without website?
